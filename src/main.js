cdc2b56287ee5064aa5ce9c414c6a36f784690ed
Limstash Wong <limstash.w@gmail.com>
2024-05-27 02:28:32 +0800

    页面二支持文件夹上传，支持区间

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import ElementUI from "element-ui";
+import uploader from 'vue-simple-uploader'
 import "element-ui/lib/theme-chalk/index.css";
 import router from "./router/index";
 import vuescroll from "vuescroll";
@@ -9,6 +10,8 @@ import store from "./store";
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 Vue.use(vuescroll);
+Vue.use(uploader);
+
 // Vue.use(axios);
 // axios.defaults.baseURL = "http://localhost:12345";
 // axios.defaults.baseURL = "http://njhlwtj.iflysec.com";
